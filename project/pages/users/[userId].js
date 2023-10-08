6b3166e3aa9db933e713855af157e0fe2e1a6712
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-08 16:58:21 +0330

    fallback true

--- project/pages/users/[userId].js
+++ project/pages/users/[userId].js
@@ -1,6 +1,11 @@
 import React from 'react';
+import {useRouter, userRouter} from 'next/router';
 
 const UserDetails = ({data}) => {
+    const router = useRouter();
+    if(router.isFallback){
+        return <h1>is FallBack!</h1>
+    }
     return (
         <div>
             <h1>UserDetail</h1>
@@ -16,12 +21,16 @@ export async function getStaticPaths(){
 
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
+    const userData = data.slice(0,4);
 
-    const paths = data.map(user => ({params:{userId:user.id.toString()}}))
+   // const paths = data.map(user => ({params:{userId:user.id.toString()}}))
+   const paths = userData.map(user=>({params:{userId:user.id.toString()}}))
 
     return{
         paths,
-        fallback:false,
+        //fallback:false,
+        fallback:true,
+
     }
 }
 
@@ -29,6 +38,10 @@ export async function getStaticProps(context){
     const {params} = context;
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
     const data = await res.json();
+
+    if(!data.name){
+        return {notFound:true}
+    }
 
     return{
         props:{
