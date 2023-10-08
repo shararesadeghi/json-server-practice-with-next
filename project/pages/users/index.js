104f0bbc05670f51b31e57d1f82a6cd3e7c90d08
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-08 15:39:56 +0330

    create users& dynamic route for user

---
+++ project/pages/users/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link;'
+
+const Users = ({users}) => {
+    return (
+        <div>
+            <ul>
+            {users.map(user=>(
+                <li key={user.id}>
+                    <Link href={`/users/${user.id}`}>{user.name}</Link>
+                </li>
+            ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Users;
+
+export async function getStaticProps(){
+
+     const res = await fetch("https://jsonplaceholder.typicode.com/users");
+     const data = await res.json();
+    return {
+        props:{users:data}
+    }
+}
