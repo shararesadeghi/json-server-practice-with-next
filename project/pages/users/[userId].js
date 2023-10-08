de48103fe08e65513d66bc53a99134f0e9f5da58
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-08 16:04:04 +0330

    use getStatisPaths in dynamicroutes

--- project/pages/users/[userId].js
+++ project/pages/users/[userId].js
@@ -1,12 +1,38 @@
 import React from 'react';
 
-const UserDetails = () => {
+const UserDetails = ({data}) => {
     return (
         <div>
-            
+            <h1>UserDetail</h1>
+            <h3>{data.name}</h3>
+            <h3>{data.email}</h3>
         </div>
     );
 };
 
 export default UserDetails;
 
+export async function getStaticPaths(){
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    const paths = data.map(user => ({params:{userId:user.id.toString()}}))
+
+    return{
+        paths,
+        fallback:false,
+    }
+}
+
+export async function getStaticProps(context){
+    const {params} = context;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
+    const data = await res.json();
+
+    return{
+        props:{
+            data
+        }
+    }
+}
