43a0b95f37f8c3d6a149d033015a743ac6b0d496
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-09 17:32:59 +0330

    use redirect & notFound in SSR

--- project/pages/albums/[albumId].js
+++ project/pages/albums/[albumId].js
@@ -16,6 +16,12 @@ export async function getServerSideProps(context){
     const res = await fetch(`http://localhost:4000/albums/${params.albumId}`);
     const data = await res.json();
 
+    if(!data.title){
+        return{
+            //notFound:true
+            redirect:{destination:"/albums"}
+        }
+    }
     return{
         props:{
             album:data
