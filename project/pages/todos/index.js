6bd6ef012af6bbfee0c7d6588a7f89b35d1c2ea0
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-10-10 15:21:20 +0330

    add useSWR instead of useEffect for datafetching

--- project/pages/todos/index.js
+++ project/pages/todos/index.js
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
+import React from "react";
+import useSWR from 'swr';
+
+const url = "http://localhost:4000/todos";
+
+const fetcher = url => fetch(url).then(res=>res.json());
 
 const Todos = () => {
 
-    const [todos, setTodos] = useState([]);
+    // const [todos, setTodos] = useState([]);
 
-    useEffect(()=>{
-        fetch("http://localhost:4000/todos")
-            .then(res=>res.json())
-            .then(data=>setTodos(data))
-    },[])
+    // useEffect(()=>{
+    //     fetch("http://localhost:4000/todos")
+    //         .then(res=>res.json())
+    //         .then(data=>setTodos(data))
+    // },[])
+
+    const {data, error} = useSWR(url, fetcher);  
+    
     return (
         <div>
-           {todos.length ? (todos.map(todo=><h3 key={todo.id}>{todo.title}</h3>)): (<h1>Loading...</h1>)} 
+            {data ? (data.map(todo=><h3 key={todo.id}>{todo.title}</h3>)):(<h1>Loading...</h1>)}
+           {/* {todos.length ? (todos.map(todo=><h3 key={todo.id}>{todo.title}</h3>)): (<h1>Loading...</h1>)}  */}
+
         </div>
     );
 };
